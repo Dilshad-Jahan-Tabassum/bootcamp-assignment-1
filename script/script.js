9d04e819dc828760350138785ad13300c352f1dc
Dilshad-Jahan-Tabassum <dilsadtabassum73@gmail.com>
2026-02-17 16:12:58 +0600

    trending done

---
+++ script/script.js
@@ -0,0 +1,9 @@
+// const loadProducts = () =>{
+//     fetch('https://fakestoreapi.com/products')
+//     .then(res=>res.json())
+//     .then(data=>console.log(data.data))
+// }
+// const showProducts = products =>{
+
+// }
+// loadProducts();
